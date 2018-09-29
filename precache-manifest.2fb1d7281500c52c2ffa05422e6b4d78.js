e43fccddc7fee9012da008117a02b3ab6dc74bcf
Mike Starr <mstarr@rsui.com>
2018-09-29 10:00:42 -0400

    update site

---
+++ precache-manifest.2fb1d7281500c52c2ffa05422e6b4d78.js
@@ -0,0 +1,36 @@
+self.__precacheManifest = [
+  {
+    "url": "674f50d287a8c48dc19ba404d20fe713.eot"
+  },
+  {
+    "revision": "4cb23d2db71cb6a9f7dec34bebb809b9",
+    "url": "index.html"
+  },
+  {
+    "url": "polyfills.8fa428b4c7be499a7d64.js"
+  },
+  {
+    "url": "main.8fa428b4c7be499a7d64.css"
+  },
+  {
+    "url": "main.8fa428b4c7be499a7d64.js"
+  },
+  {
+    "url": "vendor.8fa428b4c7be499a7d64.js"
+  },
+  {
+    "url": "fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "url": "b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "url": "912ec66d7572ff821749319396470bde.svg"
+  },
+  {
+    "url": "0.8fa428b4c7be499a7d64.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.a9c21429a040447ab04b826ff3d29299.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+importScripts("precache-manifest.2fb1d7281500c52c2ffa05422e6b4d78.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
 
 
 
